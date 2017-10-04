d01dceb4c1ae99bf755adaa66d6936026886fbb0
kbthetruth <kbthetruth23@gmail.com>
2017-10-04 15:12:39 -0600

    Rearraged some config file elements KB

--- src/edu/colostate/cs/cs414/teamthebestteam/rollerball/common/Config.java
+++ src/edu/colostate/cs/cs414/teamthebestteam/rollerball/common/Config.java
@@ -3,33 +3,22 @@ package edu.colostate.cs.cs414.teamthebestteam.rollerball.common;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Config {
 	
+	Connection conn;
+	Statement stmt;
+	ResultSet result;
+	int res;
+	
 	public Config()
 	{
-		
-	}
-	
-	public void Connect()
-	{
 		try {
-			//get database connection
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Rollerball", "root", "password");
-			
-			//create statement 
-			Statement stmt = conn.createStatement();
-			//execute query
-			ResultSet result = stmt.executeQuery("select * from users");
-			
-			while(result.next())
-			{
-				System.out.println(result.getString("nick_name") +" "+ result.getString("email"));
-			}
-			
-			//process result set
-		} catch (Exception e) {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Rollerball", "root", "password");
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}

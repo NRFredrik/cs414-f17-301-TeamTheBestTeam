d7e6c754710cbdcd6454c96738a5ef6a0cac26b7
Cody <myer62@gmail.com>
2017-10-09 13:09:20 -0600

    merged the tile classes into one file CM

--- src/edu/colostate/cs/cs414/teamthebestteam/rollerball/gameboard/Tile.java
+++ src/edu/colostate/cs/cs414/teamthebestteam/rollerball/gameboard/Tile.java
@@ -24,7 +24,7 @@ public abstract class Tile {
 	private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
 		final Map<Integer,EmptyTile> emptyTileMap = new HashMap<>();
 		for(int i = 0; i < 40; i++){
-			emptyTileMap.put(i,new EmptyTile(i));
+			emptyTileMap.put(i, new EmptyTile(i));
 		}
 		return ImmutableMap.copyOf(emptyTileMap);		
 	}
@@ -33,5 +33,39 @@ public abstract class Tile {
 	
 	public abstract Piece getPiece();
 	
+	public static final class EmptyTile extends Tile{
+		private EmptyTile(final int coordinate){
+			super(coordinate);
+		}
+
+		@Override
+		public boolean isTileOccupided() {
+			return false;
+		}
+
+		@Override
+		public Piece getPiece() {
+			return null;
+		}
+	}
+	
+	public static final class OccupiedTile extends Tile{
+		//cannot be referenced without getter
+		private final Piece pieceOnTile;
+		
+		private OccupiedTile(final int tileCoordinate, Piece pieceOnTile) {		
+			super(tileCoordinate);
+			this.pieceOnTile = pieceOnTile;
+		}
+		@Override
+		public boolean isTileOccupided() {
+			return true;
+		}
+		@Override
+		public Piece getPiece() {
+			return this.pieceOnTile;
+		}		
+
+	}
 	
 }

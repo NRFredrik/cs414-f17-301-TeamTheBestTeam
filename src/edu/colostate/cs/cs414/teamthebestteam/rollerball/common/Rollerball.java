c348080db1084115e72bc4974cee22863ee086d9
ksconner <ksconner81@yahoo.com>
2017-10-18 09:11:00 -0600

    Connected two players and got the pieces moving for two players in a game

--- src/edu/colostate/cs/cs414/teamthebestteam/rollerball/common/Rollerball.java
+++ src/edu/colostate/cs/cs414/teamthebestteam/rollerball/common/Rollerball.java
@@ -1,21 +1,61 @@
 package edu.colostate.cs.cs414.teamthebestteam.rollerball.common;
 
+import java.io.IOException;
+
+
+
 import edu.colostate.cs.cs414.teamthebestteam.rollerball.gameboard.Board;
+import edu.colostate.cs.cs414.teamthebestteam.rollerball.gui.ClientTable;
 import edu.colostate.cs.cs414.teamthebestteam.rollerball.gui.Table;
 
 
-public class Rollerball {
-	public static void main(String[] args) {
-		System.out.println();
-		//this is board from a starting position
-		Board board = Board.createStandardBoard();
-		//print board. Takes advantage of tosting() for each of the pieces 
-		//to print this string representation of the board
-		
-		//System.out.println(board);
-		
-		//Define the table to be a 600X600 table and is visible right now
-		Table table = new Table();
-	}
 
+
+public class Rollerball extends AbstractClient 
+{
+	RollIF clientUI;
+	
+	public Rollerball(String host, int port, RollIF clientUI) throws IOException 
+	{	
+		super(host, port); // Call the superclass constructor
+		openConnection();
+		this.clientUI = clientUI;
+		//handleMessageFromClientUI("login");
+		
+	}
+	@Override
+	protected void handleMessageFromServer(Object msg) 
+	{
+		clientUI.display(msg);	
+	}
+	public void handleMessageFromClientUI(Object message) 
+	{
+		try 
+		{
+			
+			sendToServer(message);
+			System.out.println("Sent");
+		} catch (IOException error) 
+		{
+			System.out.println(error);
+			quit();
+		}
+	}
+	
+	public void quit() {
+		try {
+			//handleMessageFromClientUI("#logoff ");
+			closeConnection();
+		} catch (IOException error) {
+		}
+		System.exit(0);
+	}
+	
+	
+	
+
+
+
+
+	
 }

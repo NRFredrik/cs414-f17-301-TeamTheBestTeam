996e4f1b440edfd49fc9c9666460f15b1613951a
Cody <myer62@gmail.com>
2017-10-25 14:12:31 -0600

    test suite for JUnitCM

---
+++ src/edu/colostate/cs/cs414/teamthebestteam/rollerball/test/TestAll.java
@@ -0,0 +1,17 @@
+package edu.colostate.cs.cs414.teamthebestteam.rollerball.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	BoardUtilitiesTest.class,
+	LoginTest.class,
+	RegisterTest.class
+})
+
+public class TestAll {
+
+}
